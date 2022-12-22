2c8b506ce5890ea15f9da9d32f50fb147f75c015
wdc35333 <wdc35333@gmail.com>
2022-12-22 23:59:18 +0900

    [WDC] chapter3 라우팅

---
+++ meadowlark.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// 핸들바 뷰 엔진 설정
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', { fortune: randomFortune })});
+
+// custom 404 page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+})
+
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500);
+    res.render('500');
+})
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}}; ` +
+    `press Ctrl-C to terminate.`
+))
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know",
+    "You will have a pleasant surprise",
+    "Whenever possible, keep it simple",
+]
